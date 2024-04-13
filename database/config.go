51f3b4e3f63221b3599eaebad9e91b813652b342
kawasaki <yomitanmasa3523@gmail.com>
2024-04-14 00:14:21 +0900

    config

--- database/config.go
+++ database/config.go
@@ -4,6 +4,7 @@ package database
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
+	"fmt"
 	"os"
 	"github.com/joho/godotenv"
 	"gorm.io/gorm"
@@ -19,23 +20,28 @@ func GormConnect() *gorm.DB {
 			panic ("envファイルの読み込みに失敗しました。")
     }
 
-	var USER string
-	var PASS string
 	var PROTOCOL string
-	var DBNAME string
+	var PASS string
 	
 	env := os.Getenv("ENV")
-  	if env == "dev" {
-		USER = os.Getenv("DB_USER")
+    if env != "local" {
+        PROTOCOL = os.Getenv("DB_PROTOCOL_LOCAL")
+		PASS = ""
+    } else {
+        PROTOCOL = os.Getenv("DB_PROTOCOL")
 		PASS = os.Getenv("DB_PASS")
-		DBNAME = os.Getenv("DB_DATABASE")
-		PROTOCOL = os.Getenv("DB_PROTOCOL")
-	}
+    }
+
+    USER := os.Getenv("DB_USER")
+    DBNAME := os.Getenv("DB_DATABASE")
+
+	fmt.Println("Database connection info:", USER, PASS, DBNAME, PROTOCOL)
 
 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err2 := gorm.Open(mysql.Open(CONNECT), &gorm.Config{})
-	if err2 != nil {
-		panic ("データベースとの通信に失敗しました。")
+	db, err := gorm.Open(mysql.Open(CONNECT), &gorm.Config{})
+
+	if err != nil {
+		panic (err)
 	}
 	
 	return db
