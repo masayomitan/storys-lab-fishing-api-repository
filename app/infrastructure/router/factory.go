bebc3314deab697e2257763dc3582a2f2687e2f0
kawasaki <yomitanmasa3523@gmail.com>
2024-04-21 13:59:54 +0900

    sql　edit

---
+++ app/infrastructure/router/factory.go
@@ -0,0 +1,44 @@
+package router
+
+import (
+	"errors"
+	"time"
+	"github.com/gin-gonic/gin"
+	// "storys-lab-fishing-api/adapter/repository"
+	// "storys-lab-fishing-api/adapter/logger"
+	// "storys-lab-fishing-api/adapter/validator"
+)
+
+type Server interface {
+	Listen()
+}
+
+type Port int
+
+const (
+	InstanceGorillaMux int = iota
+	InstanceGin
+)
+
+var (
+	errInvalidWebServerInstance = errors.New("invalid router server instance")
+)
+
+func NewWebServerFactory(
+	instance int,
+	// log logger.Logger,
+	// dbSQL repository.SQL,
+	// validator validator.Validator,
+	port Port,
+	ctxTimeout time.Duration,
+) (Server, error) {
+	return newGinServer(
+		gin.New(),
+		// log, 
+		// dbSQL, 
+		// validator, 
+		port, 
+		ctxTimeout,
+	), 
+	nil
+}
