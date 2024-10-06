d894acf5a8c3fbdbf0d1f61ff79b7bf5bea67897
kawasaki <yomitanmasa3523@gmail.com>
2024-10-06 15:02:37 +0900

    prefeture

--- app/infrastructure/database/migrations/000008_create_prefectures.up.sql
+++ app/infrastructure/database/migrations/000008_create_prefectures.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE prefectures (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) COMMENT '都道府県',
     name_kana VARCHAR(255) COMMENT '都道府県(カナ)',
+    image_url VARCHAR(255) NOT NULL COMMENT '画像保存URL',
     created_at DATETIME NOT NULL COMMENT '作成日時',
     updated_at DATETIME NOT NULL COMMENT '更新日時',
     deleted_at DATETIME DEFAULT NULL COMMENT '削除日時'
